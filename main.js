fd81517572561d6feb11ff113df3b2099c14474f
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-21 12:52:01 +0300

    Import linked list class, implement increaseCapacity method

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-class HashMap {
-    constructor(loadFactor, capacity = 1) {}
+import { LinkedList } from './linked-lists.js';
 
-    hash(key) {}
+class HashMap {
+    constructor(loadFactor = 0.8, capacity = 16) {}
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+        }
+        return hashCode;
+    }
+    increaseCapacity() {
+        this.capacity *= 2;
+    }
     set(key, value) {}
     get(key) {}
     has(key) {}
