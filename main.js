8df9261b2c04c62ed17fe430863d30a4e128cffd
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-21 13:07:31 +0300

    Import Node class

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-import { LinkedList } from './linked-lists.js';
+import { LinkedList, Node } from './linked-lists.js';
 
 class HashMap {
-    constructor(loadFactor = 0.8, capacity = 16) {}
+    constructor(loadFactor = 0.8, capacity = 16, buckets = []) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        this.buckets = buckets;
+    }
 
+    // hash function from TOP
     hash(key) {
         let hashCode = 0;
 
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
-            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+            hashCode =
+                (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
         }
         return hashCode;
     }
+
     increaseCapacity() {
         this.capacity *= 2;
     }
+
     set(key, value) {}
+
     get(key) {}
     has(key) {}
     remove(key) {}
