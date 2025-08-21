d0d51cad621645f2f64b916cbc4a1ce59d8f9a10
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-21 17:08:25 +0300

    Implement buildTree method which returns the main root of the tree

--- binary-search-trees/main.js
+++ binary-search-trees/main.js
@@ -14,13 +14,32 @@ class Tree {
 
     // Create a balanced binary tree full of Node objects. Return the level - 0 root node.
     buildTree(array) {
+        // sort and remove dupes
         array = array.sort((a, b) => a - b);
-        array = [ ...new Set(array) ];
+        array = [...new Set(array)];
 
+        // if it's the last node
+        if (array.length === 0) {
+            return null;
+        }
+
+        // else, slice the array into 2 sub-arrays, run buildTree on left sides, then right side
+        const start = 0;
+        const end = array.length - 1;
+        const middle = Math.floor((start + end) / 2);
+
+        const currRoot = new Node(array[middle]);
+
+        const leftSubarray = array.slice(0, middle);  
+        const rightSubarray = array.slice(middle + 1);  
+
+        currRoot.left = buildTree(leftSubarray);
+        currRoot.right = buildTree(rightSubarray);
+
+        return currRoot;
     }
 
     insert(value) {}
-
     deleteItem(value) {}
     find(value) {}
     levelOrderForEach(callback) {}
