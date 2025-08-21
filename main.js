7582331ab74b78f399a8fb38479abffda0bf89c2
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-08-21 12:37:04 +0300

    Set up skeleton code for hashmap class

---
+++ main.js
@@ -0,0 +1,14 @@
+class HashMap {
+    constructor(loadFactor, capacity = 1) {}
+
+    hash(key) {}
+    set(key, value) {}
+    get(key) {}
+    has(key) {}
+    remove(key) {}
+    length() {}
+    clear() {}
+    keys() {}
+    values() {}
+    entries() {}
+}
